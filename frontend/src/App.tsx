ef3ad6d82214f0e86e428d813198ef851cb83abf
Abubakar Minhas <jakeminhas91@gmail.com>
2025-04-04 18:20:56 +0500

    hammad and mine updated tsx code, now working on dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Dashboard from "./components/Dashboard";
-import NoteEditor from './components/NoteEditor';
-
-
 
 function App() {
   return (
@@ -12,8 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dummy" element={<Dashboard />} />
-        <Route path="/notes/:id/edit" element={<NoteEditor />} />
       </Routes>
     </Router>
   );
